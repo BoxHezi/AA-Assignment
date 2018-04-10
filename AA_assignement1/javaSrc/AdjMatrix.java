ef372898a23af8998d3cca6fac4e8eb3112c14f6
Boxuan Lu <547181293@qq.com>
2018-04-10 15:34:19 +1000

    Add files via upload

--- AA_assignement1/javaSrc/AdjMatrix.java
+++ AA_assignement1/javaSrc/AdjMatrix.java
@@ -9,116 +9,176 @@ import java.util.*;
  * @author Jeffrey Chan, 2016.
  */
 public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
-    private ArrayList<T> vertexList;
-    private HashMap<T, ArrayList<T>> edgeMap;
-
-    private int[][] matrix;
+    private ArrayList<T> vertex;
+    private Integer[][] matrix;
 
     /**
      * Contructs empty graph.
      */
     public AdjMatrix() {
         // Implement me!
-        vertexList = new ArrayList<>();
-        edgeMap = new HashMap<>();
-
-        matrix = new int[0][0];
+        vertex = new ArrayList<>();
+        matrix = new Integer[vertex.size()][vertex.size()];
+        for (int i = 0; i < vertex.size(); i++) {
+            for (int j = 0; j < vertex.size(); j++) {
+                matrix[i][j] = 0;
+            }
+        }
     } // end of AdjMatrix()
-
 
     public void addVertex(T vertLabel) {
         // Implement me!
-        if (vertexList.contains(vertLabel)) {
+        if (vertex.contains(vertLabel)) {
             System.err.println("Vertex existed!");
             return;
         }
-        vertexList.add(vertLabel);
-        edgeMap.put(vertLabel, new ArrayList<>());
+        vertex.add(vertLabel);
 
-        /*addToMatrix();
-        setMatrix();
-        printMatrix();*/
+        addToMatrix();
     } // end of addVertex()
+
+    private void addToMatrix() {
+        int newMatrixSize = matrix.length + 1;
+        Integer[][] tempMatrix = new Integer[newMatrixSize][newMatrixSize];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                tempMatrix[i][j] = matrix[i][j];
+            }
+        }
+        tempMatrix = setDefaultMatrixValue(tempMatrix);
+        matrix = tempMatrix;
+
+        printMatrix();
+    }
+
+    private Integer[][] setDefaultMatrixValue(Integer[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (null == matrix[i][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
 
     public void addEdge(T srcLabel, T tarLabel) {
         // Implement me!
-        if (!existedVertex(srcLabel) || !existedVertex(tarLabel)) {
-            System.err.println("One of the vertex is not existed!");
+        if (!existVertex(srcLabel) || !existVertex(tarLabel)) {
+            System.err.println("One of the vertex doesn't existed!");
             return;
         }
-        ArrayList<T> tempEdgeList = edgeMap.get(srcLabel);
-        tempEdgeList.add(tarLabel);
-        edgeMap.put(srcLabel, tempEdgeList);
+        int srcIndex = findVertexIndex(srcLabel);
+        int tarIndex = findVertexIndex(tarLabel);
 
-        tempEdgeList = edgeMap.get(tarLabel);
-        tempEdgeList.add(srcLabel);
-        edgeMap.put(tarLabel, tempEdgeList);
+        // set both direction in the matrix graph
+        matrix[srcIndex][tarIndex] = 1;
+        matrix[tarIndex][srcIndex] = 1;
+
+        printMatrix();
     } // end of addEdge()
 
-
     public ArrayList<T> neighbours(T vertLabel) {
-        ArrayList<T> neighbours = new ArrayList<T>();
+        ArrayList<T> neighbours = new ArrayList<>();
         // Implement me!
-        neighbours = edgeMap.get(vertLabel);
+        if (!existVertex(vertLabel)) {
+            System.err.println("Vertex doesn't existed!");
+            return null;
+        }
+        int vertIndex = findVertexIndex(vertLabel);
+        for (int i = 0; i < vertex.size(); i++) {
+            if (matrix[vertIndex][i] == 1) {
+                neighbours.add(vertex.get(i));
+            }
+        }
+
         return neighbours;
     } // end of neighbours()
 
     public void removeVertex(T vertLabel) {
         // Implement me!
-        if (!existedVertex(vertLabel)) {
+        if (!existVertex(vertLabel)) {
             System.err.println("Vertex doesn't existed!");
             return;
         }
+        System.out.println();
+        int removeVertexIndex = findVertexIndex(vertLabel);
+        Integer[][] newMatrix = new Integer[vertex.size() - 1][vertex.size() - 1];
 
-        // remove edge if there is other vertex connect to this vertex
-        for (T currentVertex : vertexList) {
-            ArrayList<T> tempEdgeList = edgeMap.get(currentVertex);
-            if (tempEdgeList.contains(vertLabel)) {
-                tempEdgeList.remove(vertLabel);
-                edgeMap.put(currentVertex, tempEdgeList);
+        // move the vertex need to be removed at the end of every array
+        // therefore, when declare new array, just ignore the last item of each array
+        for (int i = 0; i < matrix.length; i++) {
+            if (i >= removeVertexIndex) {
+                if (i >= matrix.length - 1) {
+                    break;
+                }
+                Integer[] tempArray = matrix[i];
+                matrix[i] = matrix[i + 1];
+                matrix[i + 1] = tempArray;
+            }
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (j >= removeVertexIndex) {
+                    if (j >= matrix[i].length - 1) {
+                        break;
+                    }
+                    Integer tempInt = matrix[i][j];
+                    matrix[i][j] = matrix[i][j + 1];
+                    matrix[i][j + 1] = tempInt;
+                }
+            }
+        }
+        for (int i = 0; i < newMatrix.length; i++) {
+            for (int j = 0; j < newMatrix[i].length; j++) {
+                newMatrix[i][j] = matrix[i][j];
             }
         }
 
-        vertexList.remove(vertLabel);
-        edgeMap.remove(vertLabel);
+        matrix = newMatrix;
+        vertex.remove(vertLabel);
+
+        printMatrix();
     } // end of removeVertex()
 
     public void removeEdge(T srcLabel, T tarLabel) {
         // Implement me!
-        if (!existedVertex(srcLabel) || !existedVertex(tarLabel)) {
+        if (!existVertex(srcLabel) || !existVertex(tarLabel)) {
             System.err.println("One of the vertex doesn't existed!");
             return;
         }
+        int srcIndex = findVertexIndex(srcLabel);
+        int tarIndex = findVertexIndex(tarLabel);
 
-        ArrayList<T> edgeList = edgeMap.get(srcLabel);
-        edgeList.remove(tarLabel);
-        edgeMap.put(srcLabel, edgeList);
+        // set both direction in the matrix graph
+        matrix[srcIndex][tarIndex] = 0;
+        matrix[tarIndex][srcIndex] = 0;
 
-        edgeList = edgeMap.get(tarLabel);
-        edgeList.remove(srcLabel);
-        edgeMap.put(tarLabel, edgeList);
+        printMatrix();
     } // end of removeEdges()
 
     public void printVertices(PrintWriter os) {
         // Implement me!
-        for (T vertex : vertexList) {
-            os.print(vertex);
-            os.print(" ");
+        for (T currVertex : vertex) {
+            os.print(currVertex + " ");
         }
+        os.println();
         os.close();
     } // end of printVertices()
 
     public void printEdges(PrintWriter os) {
         // Implement me!
-        for (int i = 0; i < vertexList.size(); i++) {
-            T currentVertex = vertexList.get(i);
-            ArrayList<T> tempEdgeList = edgeMap.get(currentVertex);
-            if (tempEdgeList.size() <= 0) {
+        for (int i = 0; i < matrix.length; i++) {
+            ArrayList<Integer> printList = new ArrayList<>();
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    printList.add(j);
+                }
+            }
+            if (printList.size() <= 0) {
                 continue;
             }
-            os.print(currentVertex);
-            for (int j = 0; j < tempEdgeList.size(); j++) {
-                os.print(" " + tempEdgeList.get(j));
+            os.print(vertex.get(i));
+            for (Integer printIndex : printList) {
+                os.print(" " + vertex.get(printIndex));
             }
             os.println();
         }
@@ -127,46 +187,48 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
 
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
         // Implement me!
-        if (!existedVertex(vertLabel1) || !existedVertex(vertLabel2)) {
+
+        if (!existVertex(vertLabel1) || !existVertex(vertLabel2)) {
             System.err.println("One of the vertex doesn't existed!");
-            return disconnectedDist;
-        }
-        if (isolateVertex(vertLabel1) || isolateVertex(vertLabel2)) {
-            return disconnectedDist;
         }
 
-        int shortestPath = 0;
         boolean found = false;
-        ArrayList<T> queue = edgeMap.get(vertLabel1);
-        //get queue size
-        int queueSize = queue.size();
+        int shortestPath = 0;
+
+        ArrayList<T> visitedList = new ArrayList<>();
+        ArrayList<T> queue = new ArrayList<>();
+        ArrayList<T> nextQueue = new ArrayList<>();
+        int currVertIndex = findVertexIndex(vertLabel1);
+
+        // get the fisrt queue by using vertLabel1 passed in
+        for (int i = 0; i < matrix[currVertIndex].length; i++) {
+            if (matrix[currVertIndex][i] == 1) {
+                queue.add(vertex.get(i));
+            }
+        }
+
         while (!found) {
             shortestPath++;
             if (queue.size() == 0) {
                 break;
-            }
-            if (queue.contains(vertLabel2)) {
-                found = true;
             } else {
-                ArrayList<T> subQueue = new ArrayList<>();
-                for (T nextVisit : queue) {
-                    ArrayList<T> tempQueue = edgeMap.get(nextVisit);
-                    subQueue.addAll(tempQueue);
-                }
-                for (T nextQueueVertex : subQueue) {
-                    if (!queue.contains(nextQueueVertex)) {
-                        queue.add(nextQueueVertex);
+                if (queue.contains(vertLabel2)) {
+                    found = true;
+                } else {
+                    for (int j = 0; j < queue.size(); j++) {
+                        visitedList.add(vertex.get(j));
+                        for (int x = 0; x < matrix[j].length; x++) {
+                            if (matrix[j][x] == 1 && !visitedList.contains(vertex.get(x))) {
+                                nextQueue.add(vertex.get(x));
+                            }
+                        }
                     }
+                    // reset queue and add new queue into it
+                    queue = clearQueue();
+                    queue.addAll(nextQueue);
+                    nextQueue = clearQueue();
                 }
-                /*System.out.println(queueSize);
-                System.out.println(queue.size());
-                System.out.println("vertex1: " + vertLabel1 + " vertex2: " + vertLabel2 + " " + queue);*/
             }
-        }
-
-        // remove edgeInfo for vertex as the list is changed when processing BFS
-        for (int i = queue.size() - 1; i >= queueSize; i--) {
-            queue.remove(i);
         }
 
         if (found) {
@@ -176,37 +238,37 @@ public class AdjMatrix<T extends Object> implements FriendshipGraph<T> {
         return disconnectedDist;
     } // end of shortestPathDistance()
 
-    private boolean existedVertex(T vertLabel) {
-        return vertexList.contains(vertLabel);
-    }
-
-    private boolean isolateVertex(T vertLabel) {
-        ArrayList<T> edgeList = edgeMap.get(vertLabel);
-        return edgeList.size() == 0;
-    }
-
-    private void addToMatrix() {
-        int size = matrix.length;
-        matrix = new int[size + 1][size + 1];
-    }
-
-    private void setMatrix() {
-        int size = matrix.length;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = 0;
+    private int findVertexIndex(T tarVertex) {
+        for (int i = 0; i < vertex.size(); i++) {
+            if (vertex.get(i).equals(tarVertex)) {
+                return i;
             }
         }
+        return -1;
     }
 
+    // check if a vertex existed or not
+    private boolean existVertex(T vertLabel) {
+        return vertex.contains(vertLabel);
+    }
+
+    // test output matrix
     private void printMatrix() {
-        for (int[] vertex : matrix) {
+        System.out.print("  ");
+        for (T currVertex : vertex) {
+            System.out.print(currVertex + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(vertex.get(i) + " ");
             for (int j = 0; j < matrix.length; j++) {
-                System.out.print(vertex[j]);
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
     }
 
+    private ArrayList<T> clearQueue() {
+        return new ArrayList<>();
+    }
 } // end of class AdjMatrix
