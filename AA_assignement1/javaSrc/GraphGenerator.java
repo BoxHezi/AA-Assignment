5f7e62e5dafb664af5027bbb97362534a7bc6eb0
rmit-s3658769-Simon-Lay <30329649+rmit-s3658769-Simon-Lay@users.noreply.github.com>
2018-04-10 23:06:14 +1000

    Add files via upload

--- AA_assignement1/javaSrc/GraphGenerator.java
+++ AA_assignement1/javaSrc/GraphGenerator.java
@@ -4,10 +4,14 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 
 public class GraphGenerator {
     private static final String ORIGIN_DATE_FILE = "facebook_combined.txt";
     private static final String AV_COMMAND_IN = "av.in";
+	private static final String AE1_COMMAND_IN = "aeH.in";
+	private static final String AE2_COMMAND_IN = "aeM.in";
+	private static final String AE3_COMMAND_IN = "aeL.in";
     private static final int vertexLimitation = 800;
 
     public static void main(String[] args) {
@@ -49,6 +53,7 @@ public class GraphGenerator {
         }
         // output a AV command file
         printToFile(AV_COMMAND_IN, vertexList, "AV");
+		printToFileAE(AE1_COMMAND_IN, vertexList, "AE");
     }
 
     /**
@@ -71,4 +76,39 @@ public class GraphGenerator {
             printWriter.close();
         }
     }
+	
+	/**
+     * @param fileName output file name
+     * @param printElement list of command argument
+     * @param command command AV, AE, RV, RE, etc
+	 *
+     */
+	private static void printToFileAE(String fileName, List<String> vert, String command) {
+        PrintWriter printWriter = null;
+		ArrayList<String> Edges = new ArrayList<>();
+		
+		try {
+            printWriter = new PrintWriter(new FileOutputStream(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		/*uses a randomizer to select random vertices to form edges then prints
+		it to the file AE*/ 
+		Random rand = new Random();
+        int i = 0;
+        while (i != 1000) {
+        	int n = rand.nextInt(vert.size()) + 0;
+			int m = rand.nextInt(vert.size()) + 0;
+			if (!Edges.contains(vert.get(n)+" "+vert.get(m)) && !Edges.contains(vert.get(m)+" "+vert.get(n))) {
+				Edges.add(vert.get(n)+" "+vert.get(m));
+                printWriter.println(command + " " + Edges.get(i));
+				printWriter.println(command + " " + vert.get(m)+" "+vert.get(n));
+                i++;
+			}
+        }
+		printWriter.close();
+    }
+	
+	private static void printToFileRE(String fileName, List<String> Edges, String command) {
+        //TODO
 }
