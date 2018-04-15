c270f70ca9c3ee0e10cb5f100941273aceea2535
rmit-s3658769-Simon-Lay <30329649+rmit-s3658769-Simon-Lay@users.noreply.github.com>
2018-04-15 11:15:59 +1000

    Add files via upload
    commented

--- AA_assignement1/javaSrc/GraphGenerator.java
+++ AA_assignement1/javaSrc/GraphGenerator.java
@@ -8,9 +8,11 @@ import java.util.Random;
 
 public class GraphGenerator {
     private static final String BASE_GRAPH_FILE = "facebook_combined.txt";
+    /*different density graphs to generate*/
     private static final String HD_TEST_GRAPH = "HD_base_graph.txt";
     private static final String MD_TEST_GRAPH = "MD_base_graph.txt";
     private static final String LD_TEST_GRAPH = "LD_base_graph.txt";
+    /*different files required to generate for the different operations for the 3 density graphs*/
 	private static final String AV_SET = "AV_SET.in";
     private static final String AE_SET = "AE_SET.in";
 	private static final String AEmd_SET = "AEmd_SET.in";
@@ -27,18 +29,23 @@ public class GraphGenerator {
     private static final String S_SET = "S_SET.in";
 	private static final String Smd_SET = "Smd_SET.in";
 	private static final String Shd_SET = "Shd_SET.in";
+    /*used to generate the low density graph*/
     private static final int LD_INDICATOR = 3;
+    /*declaring the size of the facebook graph we want in terms of vertices*/
     private static final int VERTEX_LIMIT = 500;
+    /*declaring data size for each of the operation*/
     private static final int DATA_SIZE = 200;
 
     // store all 500 vertices in this list
     private ArrayList<String> vertexList = new ArrayList<>();
+    /* stores all vertices and edges to be used to calculate appropriate operations*/
 	private ArrayList<String> GlobalLD = new ArrayList<>();
 	private ArrayList<String> GlobalLDvert = new ArrayList<>();
 	private ArrayList<String> GlobalMD = new ArrayList<>();
 	private ArrayList<String> GlobalMDvert = new ArrayList<>();
 	private ArrayList<String> GlobalHD = new ArrayList<>();
 	private ArrayList<String> GlobalHDvert = new ArrayList<>();
+    
     // need to clean list after generate different density graph
     private ArrayList<String> tempVertexList = new ArrayList<>();
     private ArrayList<String> tempEdgeList = new ArrayList<>();
@@ -57,6 +64,7 @@ public class GraphGenerator {
         graphGenerator.generateS();
     }
 
+    /*reads the facebook graph and uses it as a base graph to gain the first few edges that contain 500 unique vertices*/
     private void getFiveHundredVertex() {
         Scanner readFile = null;
         try {
@@ -85,6 +93,7 @@ public class GraphGenerator {
         }
     }
 
+    /*generates the high density graph using the 500 vertices from reading the first 500 vertices*/
     private void createGraphHD() {
         Scanner readFile = null;
         try {
@@ -120,6 +129,7 @@ public class GraphGenerator {
         printToFile(HD_TEST_GRAPH, tempEdgeList);
     }
 
+    /*creates a medium density graph based off the base file*/
     private void createGraphMD() {
         Scanner readFile = null;
         try {
@@ -158,7 +168,7 @@ public class GraphGenerator {
         }
         printToFile(MD_TEST_GRAPH, tempEdgeList);
     }
-
+    /*creates the low density graph based off the base graph but uses a limiter*/
     private void createGraphLD() {
         Scanner readFile = null;
         try {
@@ -202,7 +212,7 @@ public class GraphGenerator {
         }
         printToFile(LD_TEST_GRAPH, tempEdgeList);
     }
-
+    /*prints the grpah data to their respective density file*/
     private void printToFile(String filename, List<String> printElement) {
         PrintWriter printWriter = null;
         try {
@@ -222,8 +232,11 @@ public class GraphGenerator {
     }
 
     /*************************************************************************************/
+    /*generating operations*/
+    
+    /*generating random numbers that exist in the base graphs to add new edges, the funciton creates
+    the data for the three different density graphs*/
     private void generateRandAE() {
-        /*this doesn't go in generateEdges becasue we want random ones to add*/
         ArrayList<String> edges = new ArrayList<>();
 		ArrayList<String> edges2 = new ArrayList<>();
 		ArrayList<String> edges3 = new ArrayList<>();
@@ -265,7 +278,8 @@ public class GraphGenerator {
 		printToFileData(AEmd_SET, edges2, "AE");
 		printToFileData(AEhd_SET, edges3, "AE");
     }
-
+    /*generating random numbers that exist in the base graphs to calculate the shortest path,
+    the funciton creates the data for the three different density graphs*/
     private void generateS() {
         ArrayList<String> edges = new ArrayList<>();
 		ArrayList<String> edges2 = new ArrayList<>();
@@ -293,7 +307,8 @@ public class GraphGenerator {
 		printToFileData(Smd_SET, edges2, "S");
 		printToFileData(Shd_SET, edges3, "S");
     }
-
+    /*generating random numbers that exist in the base graphs to remove edges, the funciton creates
+    the data for the three different density graphs*/
     private void generateRE() {
         ArrayList<String> edges = new ArrayList<>();
 		ArrayList<String> edges2 = new ArrayList<>();
@@ -333,9 +348,9 @@ public class GraphGenerator {
 		printToFileData(REmd_SET, edges2, "RE");
 		printToFileData(REhd_SET, edges3, "RE");
     }
-
+    /*generating random numbers that don't exist in the base graphs. It's using the 
+    base graph so then it doesn't need to create 3 files for each density*/
     private void generateRandAV() {
-        /*this doesn't go in generateVert becasue we want random ones to add*/
         ArrayList<String> vert = new ArrayList<>();
         Random rand = new Random();
         int i = 0;
@@ -348,19 +363,8 @@ public class GraphGenerator {
         }
         printToFileData(AV_SET, vert, "AV");
     }
-
-    private void generateVert(List<String> vert) {
-        Random rand = new Random();
-        int i = 0;
-        while (i < DATA_SIZE) {
-            int n = rand.nextInt(10000);
-            if (GlobalLDvert.contains(String.valueOf(n)) && !vert.contains(String.valueOf(n)) && GlobalMDvert.contains(String.valueOf(n))) {
-                vert.add(String.valueOf(n));
-                i++;
-            }
-        }
-    }
-
+    /*generating random numbers that exist in the base graphs to remove vertices, the funciton creates
+    the data for the three different density graphs*/
     private void generateRV() {
         ArrayList<String> vert = new ArrayList<>();
 		ArrayList<String> vert2 = new ArrayList<>();
@@ -400,7 +404,8 @@ public class GraphGenerator {
 		printToFileData(RVmd_SET, vert2, "RV");
 		printToFileData(RVhd_SET, vert3, "RV");
     }
-
+    /*generating random numbers that exist in the base graph to find neighbours, the funciton creates
+    the data for the three different density graphs*/
     private void generateN() {
         ArrayList<String> vert = new ArrayList<>();
 		ArrayList<String> vert2 = new ArrayList<>();
@@ -440,7 +445,7 @@ public class GraphGenerator {
 		printToFileData(Nmd_SET, vert2, "N");
 		printToFileData(Nhd_SET, vert3, "N");
     }
-
+    /*prints the operation to a file*/
     private void printToFileData(String filename, List<String> printElement, String command) {
         PrintWriter printWriter = null;
         try {
